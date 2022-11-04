8a64a8ab69215769971768ad951ea9c5caeea32d
guib <betwhatspok@protonmail.com>
2022-11-04 14:26:32 -0400

    premier palier

--- main.py
+++ main.py
@@ -1,72 +1,78 @@
 #!/usr/bin/python3
 import numpy as np
-# Définitions des portes quantiques à deux qubits
-i_gate = np.array([[1,0],[0,1]])
-x_gate = np.array([[0,1],[1,0]])
-h_gate = np.sqrt(0.5) * np.array([[1,1],[1,-1]])
-cx_gate = np.array([[1,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]])
-#Produits tensoriels de portes quantiques
-ih_gate = np.kron(i_gate, h_gate)
-#Paire de Bell, état produit des portes quantiques cx_gate multiplié par ih_gate
-b_circuit = cx_gate @ ih_gate
+
+# Les exceptions ne sont pas encore gérées, à utiliser avec précaution
+def normalProductOnDoors(circuit1 :np.ndarray, circuit2: np.ndarray) -> np.ndarray:
+    return circuit2 @ circuit1
+
+def tensorProductOnDoors(circuit1 :np.ndarray, circuit2: np.ndarray) -> np.ndarray:
+        return np.kron(circuit2, circuit1)
+
+#TO DO
+#Fonction qui ajoute une version contrôlé d'une porte quantique (argument1: circuit||Porte)
+
+#TO DO
+#Fonction qui réorganise les qubits d'une porte quantique (argument1: porte, argument2: ordre des qubits)
+
+
 #Définition d'un classe pour les opérations courantes
 #---------------------------------------------------------------------------------------------
 class QuantumOperations:
-    init_state = np.ndarray()
+    init_state = None
 
-    # Définitions des portes quantiques
-    i_gate_1qu = np.array([[1,0],[0,1]])
-    x_gate_1qu = np.array([[0,1],[1,0]])
-    h_gate_1qu = np.sqrt(0.5) * np.array([[1,1],[1,-1]])
-    cx_gate_2qu = np.array([[1,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]])
-
-    #Produits tensoriels de portes quantiques
-    ih_gate = np.kron(i_gate_1qu, h_gate)
-
-    #Paire de Bell, état produit des portes quantiques cx_gate multiplié par ih_gate
-    b_circuit = cx_gate @ ih_gate
-
-    # À revoir, pour donner un profit c'est pas gagné
-    def init_a_state(self, state_init_state: np.ndarray) -> np.ndarray:
+    def init_a_state(self, init_state: np.ndarray) -> np.ndarray:
         return self.state
 
-    
+    def tensorProductOnSelfAsCircuit1(self, circuit2: np.ndarray) -> np.ndarray:
+        return np.kron(circuit2, self)
 
-    #Fonction qui ajoute une version contrôlé d'une porte quantique (argument1:)
+    def tensorProductOnSelfAsCircuit2(self, circuit1: np.ndarray) -> np.ndarray:
+        return np.kron(self, circuit1)
 
-    #Fonction qui réorganise les qubits d'une porte quantique (argument1: porte, argument2: ordre des qubits)
+    def normalProductOnSelfAsCircuit1(self, circuit2: np.ndarray) -> np.ndarray:
+        return circuit2 @ self
+
+    def normalProductOnSelfAsCircuit2(self, circuit1: np.ndarray) -> np.ndarray:
+        return self @ circuit1
+# Fin Class QuantumOperations ----------------------------------------------------------------
 #----------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
 if __name__== "__main__":
-    init_state_0 = np.array([1,0,0,0]).T
-    init_state_i = np.array([1j,0,0,0]).T
+    # Définitions des portes quantiques à deux qubits
+    i_gate = np.array([[1,0],[0,1]])
+    x_gate = np.array([[0,1],[1,0]])
+    h_gate = np.sqrt(0.5) * np.array([[1,1],[1,-1]])
+    cx_gate = np.array([[1,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]])
+    #Produits tensoriels de portes quantiques
+    #Circuit de Bell, état produit des portes quantiques cx_gate multiplié par ih_gate
 
-    b_state_from_0 = b_circuit @ init_state_0
-    b_state_from_j = b_circuit @ init_state_i
-
-    probabilities_b_state_from_0  = b_state_from_0 * np.conjugate(b_state_from_0)
-    probabilities_b_state_from_j  = np.real(b_state_from_j * np.conjugate(b_state_from_j))
-    probabilities_b_state_from_0_imag  = np.imag(b_state_from_0 * np.conjugate(b_state_from_0))
-
-
-    print(b_state_from_0)
-    print(probabilities_b_state_from_0)
-    print(probabilities_b_state_from_0_imag)
-
-    print(b_state_from_j)
-    print(probabilities_b_state_from_j)
-    print(type(init_state_0))
+#-------------------------------------------------------------------------------------------------
+    # Opérations préparatoires à 1 et 2 qubits
+    # init_state_0 = np.array([1,0,0,0]).T
+    # init_state_i = np.array([1j,0,0,0]).T
+    # b_state_from_0 = b_circuit @ init_state_0
+    # b_state_from_j = b_circuit @ init_state_i
+    # probabilities_b_state_from_0  = b_state_from_0 * np.conjugate(b_state_from_0)
+    # probabilities_b_state_from_j  = np.real(b_state_from_j * np.conjugate(b_state_from_j))
+    # probabilities_b_state_from_0_imag  = np.imag(b_state_from_0 * np.conjugate(b_state_from_0))
+    # print(b_state_from_0)
+    # print(probabilities_b_state_from_0)
+    # print(probabilities_b_state_from_0_imag)
+    # print(b_state_from_j)
+    # print(probabilities_b_state_from_j)
+    # print(type(init_state_0))
+#---------------------------------------------------------------------------------------------------
+# Premier circuit, la porte du trésor
+    #Création objet
+    treasure_door = QuantumOperations()
+    #État initial 
+    treasure_door.init_a_state(np.array([1,0,0,0,0,0,0,0]))
+    #Produit tensoriel entre une porte h et une identité à un qubit 
+    ih_gate = tensorProductOnDoors(h_gate, i_gate)
+    #Circuit de Bell, état produit des portes quantiques cx_gate multiplié par ih_gate
+    b_circuit = normalProductOnDoors(ih_gate, cx_gate)
+    premierPalierTreasureDoorCircuit = normalProductOnDoors(b_circuit, h_gate)
+    # print(premierPalierTreasureDoorCircuit)
+    print(treasure_door.normalProductOnSelfAsCircuit2(premierPalierTreasureDoorCircuit))
 
 
