31b3424dd4f59bf94d767ce533746d37ef117411
guib <betwhatspok@protonmail.com>
2022-11-04 10:20:23 -0400

    ih gate

--- main.py
+++ main.py
@@ -2,8 +2,13 @@
 import numpy as np
 
 # Définitions des portes quantiques
+i_gate = np.array([[1,0],[0,1]])
+x_gate = np.array([0,1],[1,0])
 h_gate = np.sqrt(0.5) * np.array([[1,1],[1,-1]])
+cx_gate = np.array([[1,0,0,0],[0,0,0,1],[0,0,1,0],[0,1,0,0]])
 
+#Produits tensoriels de portes quantiques
+ih_gate = np.kron(i_gate, h_gate)
 
 if __name__== "__main__":
     print("hello")
