8dac42c14fbdfa59c6e8b955a6fd403bc4ae5da5
guib <betwhatspok@protonmail.com>
2022-11-04 10:12:21 -0400

    premiere gate

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("my main is ready")
-print("my main is ready on master")
+#!/usr/bin/python3
+import numpy as np
+
+# Définitions des portes quantiques
+h_gate = np.sqrt(0.5) * np.array([[1,1],[1,-1]])
+
+
+if __name__== "__main__":
+    print("hello")
+
