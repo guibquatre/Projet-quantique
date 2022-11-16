64c771fda330f5fb73e441b37c42f3dc64a12fd3
guibquatre <services.qualite.reno@gmail.com>
2022-11-16 00:21:22 -0500

    4 fonction deustch

--- projet2/main.py
+++ projet2/main.py
@@ -2,20 +2,39 @@ from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 import matplotlib.pyplot as plt 
 from qiskit.visualization import plot_histogram
 from qiskit import Aer, execute
+# from qiskit import IBMQ
+# from qiskit.provider.ibmq import least_busy
+# token = "..."
+# IBMQ.save_account(token)
+# IBMQ.load_account()
+# IBMQ.get_provider()
+# provider.backends, faire une boucle
+# def filter_fct(x):
+#     return not x.configuration().simulator
+
+# for backend in provider.backends(filters = lambda x: not x.configuration().simulator):
+#     print(backend)
+# for backend in provider.backends(filters = filter_fct):
+#     print(backend)
+# backend = least_busy(provider.backends(filters = lambda x: not x.configuration().simulator))
+
+# provider.get_backend("...")
+
+# from qiskit.tools import job_monitor
+# job = execute.(..., backend, shots = 1000)
+# job_monitor(job)
 import sys
 from functools import wraps
 
-def build_ghz_circuit(number_of_qubits):
-    qreg = QuantumRegister(number_of_qubits, "q")
-    circuit = QuantumCircuit(qreg)
-    circuit.h(qreg[0])
+# def build_ghz_circuit(number_of_qubits):
+#     qreg = QuantumRegister(number_of_qubits, "q")
+#     circuit = QuantumCircuit(qreg)
+#     circuit.h(qreg[0])
     
-    for q in range(1,number_of_qubits):
-        circuit.cx(qreg[0],qreg[q])
+#     for q in range(1,number_of_qubits):
+#         circuit.cx(qreg[0],qreg[q])
     
-    return circuit
-
-
+#     return circuit
 
 # S'assurer que l'argument est entre 0 et 3
 def number_between_0_and_3_required(f):
@@ -31,17 +50,61 @@ def number_between_0_and_3_required(f):
 @number_between_0_and_3_required
 def circuitPourLesQuatresOraclesDeDeustch(oracleNumber: int): # -> QuantumCircuit Gate
     number_of_qubits = 2
-    oracle0_circuit = QuantumCircuit(number_of_qubits)
-    oracle0_circuit.x(1) # ready
-    oracle1_circuit = QuantumCircuit(number_of_qubits)
-    oracle2_circuit = QuantumCircuit(number_of_qubits) # ready
-    oracle3_circuit = QuantumCircuit(number_of_qubits)
     # Construire les circuits quantiques pour les quatre oracles basés sur les quatre types de fonction.
- 
-    oracle1_circuit.x(0)
-    oracle1_circuit.x(1) # ready
-    oracle3_circuit.x(0)
-    oracle3_circuit.x(1)
+    #           x       qubit0 qubit1
+    # f0                0      0
+    # f1                0      1
+    # f2        f(x)    1      0
+    # f3                1      1
+
+#   f0 |0⟩ |0⟩ = |0 ⊕ f (0)⟩ |0⟩ = |0 ⊕ 0⟩ |0⟩ = |0⟩ |0⟩
+#   f0 |0⟩ |1⟩ = |0 ⊕ f (1)⟩ |1⟩ = |0 ⊕ 0⟩ |1⟩ = |0⟩ |1⟩
+#   f0 |1⟩ |0⟩ = |1 ⊕ f (0)⟩ |0⟩ = |1 ⊕ 0⟩ |0⟩ = |1⟩ |0⟩
+#   f0 |1⟩ |1⟩ = |1 ⊕ f (1)⟩ |1⟩ = |1 ⊕ 0⟩ |1⟩ = |1⟩ |1⟩ 
+    
+    if oracleNumber == 0:
+        deustch0 = QuantumCircuit(number_of_qubits)
+        deustch0.i(0) # Ne rien faire en faisant quelque chose
+        deustch0.i(0) # Ne rien faire en faisant quelque chose
+        deustch0.to_gate(label = "oracle0")
+
+#   f1 |0⟩ |0⟩ = |0 ⊕ f (0)⟩ |0⟩ = |0 ⊕ 0⟩ |0⟩ = |0⟩ |0⟩
+#   f1 |0⟩ |1⟩ = |0 ⊕ f (1)⟩ |1⟩ = |0 ⊕ 1⟩ |1⟩ = |1⟩ |1⟩
+#   f1 |1⟩ |0⟩ = |1 ⊕ f (0)⟩ |0⟩ = |1 ⊕ 0⟩ |0⟩ = |1⟩ |0⟩
+#   f1 |1⟩ |1⟩ = |1 ⊕ f (1)⟩ |1⟩ = |1 ⊕ 1⟩ |1⟩ = |0⟩ |1⟩
+    
+    elif oracleNumber == 1:
+        deustch1 = QuantumCircuit(number_of_qubits)
+        deustch1.cx(0,1)
+        deustch1.to_gate(label = "oracle1")
+
+#   f2 |0⟩ |0⟩ = |0 ⊕ f (0)⟩ |0⟩ = |0 ⊕ 1⟩ |0⟩ = |1⟩ |0⟩
+#   f2 |0⟩ |1⟩ = |0 ⊕ f (1)⟩ |1⟩ = |0 ⊕ 0⟩ |1⟩ = |0⟩ |1⟩
+#   f2 |1⟩ |0⟩ = |1 ⊕ f (0)⟩ |0⟩ = |1 ⊕ 1⟩ |0⟩ = |0⟩ |0⟩
+#   f2 |1⟩ |1⟩ = |1 ⊕ f (1)⟩ |1⟩ = |1 ⊕ 0⟩ |1⟩ = |1⟩ |1⟩ 
+
+    elif oracleNumber == 2:
+        deustch2 = QuantumCircuit(number_of_qubits)
+        deustch2.x(0)
+        deustch2.cx(0,1)
+        deustch2.x(0)
+        deustch2.to_gate(label = "oracle2")
+
+#   f3 |0⟩ |0⟩ = |0 ⊕ f (0)⟩ |0⟩ = |0 ⊕ 1⟩ |0⟩ = |1⟩ |0⟩
+#   f3 |0⟩ |1⟩ = |0 ⊕ f (1)⟩ |1⟩ = |0 ⊕ 1⟩ |1⟩ = |1⟩ |1⟩
+#   f3 |1⟩ |0⟩ = |1 ⊕ f (0)⟩ |0⟩ = |1 ⊕ 1⟩ |0⟩ = |0⟩ |0⟩
+#   f3 |1⟩ |1⟩ = |1 ⊕ f (1)⟩ |1⟩ = |1 ⊕ 1⟩ |1⟩ = |0⟩ |1⟩
+
+    elif oracleNumber == 3:
+        deustch3 = QuantumCircuit(number_of_qubits)
+        deustch3.x(1)
+        deustch3.to_gate(label = "oracle3")
+
+    else:
+        print("problème avec le décorateur number_between_0_and_3_required")
+        sys.exit()
+
+
 
 
     # — Construire le circuit quantique pour l’algorithme de Deustch à partir de la porte quantique obtenue
